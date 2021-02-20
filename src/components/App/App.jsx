21931c45174519cd1299e0a2a6c0b5362bb12058
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-20 15:10:45 -0600

    initial commit for the start of the project, added the header as a test and had to start over due to a broken npm install

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Header from '../Header/Header'
 import './App.css';
 
 function App() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
+        <Header />
+
         <p>Gallery goes here</p>
+
         <img src="images/goat_small.jpg"/>
+
       </div>
     );
 }
