93862d62e852157f5365d5f8518fc9f75ea86b68
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-21 17:21:35 -0600

    got the div to work with the vlick but the information wont swap, need to add photos

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -15,6 +15,24 @@
 
  function GalleryItem ({photo, getGalleryList}) {
 
+  // this is the click event counter, this will hold the number of clicks
+  // create a useState function to hold the start of a number value;
+  const [toggleTrack, setTrackToggle] = useState(2);
+
+  // this is the function variable that will be called to inject
+  // or swap the photo.path to photo.description and back again
+  const tileSwap = () => {
+    console.log('inside of the tileSwap!');
+    console.log('this is toggle track', toggleTrack, typeof toggleTrack);
+    // this makes it so that every other click is added and divided so
+    // that there is always an even number
+    // use a ternary operator for the purpose of react
+    // this needs to be practice more, forget how to make these
+    return (
+      toggleTrack % 2 === 0 ?  (<p>photo.path</p>) :  (<p>photo.description</p>)
+    )
+  }
+
     // const addLikes = (prop_likes) => {
     //   let likesId = prop_likes.GalleryList.id;
     //   console.log('the like button works', target.id);
@@ -32,8 +50,6 @@
     const likesHandler = () => {
       //e.preventDefault();
       console.log('like button was clicked');
-      console.log('a', photo.id);
-      console.log('b', photo.likes);
       // axios GET request for database information of GalleryList 
         axios
           .put(`/gallery/like/${photo.id}`)
@@ -47,13 +63,29 @@
     }
 
    return (<>
+      <div onClick={() => tileSwap(setTrackToggle(toggleTrack + 1))}>
       <img src={photo.path} />
+      </div>
+          
       <p>{photo.likes}</p>
       {/* How do I grab the photo.likes onClick Handler to take the
           photo.likes++ operator to append it to the DOM, I need to use
           props for this, but I how do I interpret that */}
+
       {/* <p>{photo.likes}={likesHandler}</p> */}
-      <p>{photo.description}</p>
+
+      <p>
+        {photo.description}
+      </p>
+
+      {/* 
+        1. we need to call the function to picture swap here
+        2. on the click of the photo.path, it will swap to the photo.description
+        3. on the click of the photo.description, it will swap to the photo.path
+        4. it needs a way to keep track the click function so that the picture will have
+        5. we need to describe 
+      */}
+
       <p>
         {/* why do I need to add a function inside of the  */}
         {/* when using the onClick=() method in a button to call a function
